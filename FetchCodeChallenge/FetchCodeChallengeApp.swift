23ba963b059a068b4ea83e6c247e7313d5ad0068
lrbl <luis.lemini.98@gmail.com>
2023-09-17 22:35:04 -0600

    Initial Commit

---
+++ FetchCodeChallenge/FetchCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodeChallengeApp.swift
+//  FetchCodeChallenge
+//
+//  Created by Luis Roberto Blancas Lemini on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
